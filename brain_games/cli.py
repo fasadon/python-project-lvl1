9b3a17b135f6331230cce20cb2adea34e691ce07
Евгений Николаев <saqatod@gmail.com>
2021-03-29 19:02:23 +0300

    fix doc

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,10 @@
+"""Interface for braingames."""
+
+
 import prompt
 
+
 def welcome_user():
-	name = prompt.string('May I have your name? ')
-	print(f'Hello, {name}!')
+    """Hello with the user."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {name}!'.format(name=name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
+"""Main program."""
+
 
 from brain_games.cli import welcome_user
 
+
 def hello():
-	print('Welcome to the Brain Games!')
+    """Hello for user."""
+    print('Welcome to the Brain Games!')
+
 
 def main():
-	hello()
-	welcome_user()
-	
+    """User interface."""
+    hello()
+    welcome_user()
+
+
 if __name__ == '__main__':
-	main()
+    main()
